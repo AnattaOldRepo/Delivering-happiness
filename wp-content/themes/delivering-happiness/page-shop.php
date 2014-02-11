315f2548a4fa7e5227e5fd053b4ba60507e040c8
monkviper <surinder@anattadesign.com>
2014-02-12 03:09:41 +0530

    create markup of shop page

---
+++ wp-content/themes/delivering-happiness/page-shop.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Template Name: shop
+*/
+get_header();
+
+?>
+
+<!-- Work page-->
+<div class="shop">
+
+	<section class="two-column hero">
+		<div class="left-expanded green-area">
+			<div class="left-col">
+				<article class="content">
+					<blockquote>Too much of this is a<br/>good thing.</blockquote>
+					<span>Created with love, made with care,<br/>infused with joy.</span>
+					<a href="#" class="button">SHOP THE COLLECTION</a>
+				</article>
+			</div>
+		</div>
+		<div class="right-expanded" style="background:#dde6ef;/* It will be removed */"> 
+			<div class="right-col">
+				<div class="media">
+					<img src="<?php bloginfo('template_url')?>/assets/images/global/shop-hero.jpg" alt="" />
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="rounded-block happy-work">
+		<div class="wrapper">
+			<div class="container">
+				<article class="content">
+					<h2>Do Good, Feel Good.<span>We design products to Inspire, Connect, Educate and Experience.</span></h2>
+					<p>shop@DH goods spread and inspire happiness at home, at work, in our<br/>communities, and in our everyday lives. They aspire to make people feel a<br/>certain way. Like they just discovered their car can fly—and now spend their<br/>work commutes soaring over traffic jams, listening to fun music and eating </p>
+					<a href="#" class="button green">Visit the shop</a>
+					<img src="<?php bloginfo('template_url') ?>/assets/images/global/go-good-feel-good.jpg" alt="" />
+				</article>
+			</div>
+		</div>
+	</section>
+
+	<section class="two-column inspiration">
+		<div class="left-expanded offwhite-area">
+			<div class="left-col">
+				<div class="media">
+					<img src="<?php bloginfo('template_url') ?>/assets/images/global/customized-products.jpg" alt="" />
+				</div>
+			</div>
+		</div>
+		<div class="right-expanded strongyellow-area">
+			<div class="right-col">
+				<div class="content">
+					<h3>Inspire others with<br/>customized products.<span>We have dozens of items<br/>available for your company,<br/>team, or yourself. Get in touch<br/>and we’ll design something<br/>special, just for you</span></h3>
+					<a href="#" class="button softyellow">Share the photo</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="products-animation">
+		<div class="wrapper">
+			<h2>Our collection has style:</h2>
+			<img src="<?php bloginfo('template_url') ?>/assets/images/global/demo-products.jpg" alt="" />
+		</div>
+	</section>
+
+	<section class="three-column green get-involved">
+		<div class="wrapper">
+			<h2>Show how happy you are:</h2>
+			<div class="col left-col">
+				<div class="media">
+					<img src="<?php bloginfo('template_url') ?>/assets/images/global/c-gifts.jpg" alt="" />
+				</div>
+				<div class="gifts">
+					<div class="content small">
+						<div class="v-aling"><h3>Corporate gifts</h3></div>
+					</div>
+				</div>
+			</div>
+			<div class="col middle-col">
+				<div class="become-partner-product">
+					<div class="content">
+						<div class="v-aling"><h3>Become a<br/>partner</h3></div>
+					</div>
+				</div>
+			</div>
+			<div class="col right-col">
+				<div class="retailers">
+					<div class="media">
+						<img src="<?php bloginfo('template_url') ?>/assets/images/global/retailers.jpg" alt="" />
+					</div>
+					<div class="content small">
+						<div class="v-aling"><h3>Retailers</h3></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+</div>
+<!-- /Work page-->
+
+<?php get_footer(); ?>
