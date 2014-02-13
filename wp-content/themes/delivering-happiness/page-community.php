f85f3edd8943517a53340daf5ba74f977ebe6c67
monkviper <surinder@anattadesign.com>
2014-02-14 03:30:26 +0530

    create markup for community page

---
+++ wp-content/themes/delivering-happiness/page-community.php
@@ -0,0 +1,148 @@
+<?php
+/*
+Template Name:	community
+*/
+get_header();
+
+?>
+
+<!-- Work page-->
+<div class="community">
+
+<section class="two-column hero">
+		<div class="left-expanded orange-area">
+			<div class="left-col">
+				<article class="content">
+					<blockquote>Delivering Happiness.</blockquote>
+					<span>A global  movement,<br/>fueled by you.</span>
+					<a href="#" class="button">FIND OUT HOW</a>
+				</article>
+			</div>
+		</div>
+		<div class="right-expanded" style="background:#dde6ef;/* It will be removed */"> 
+			<div class="right-col">
+				<div class="media">
+					<img src="<?php bloginfo('template_url')?>/assets/images/global/community-hero.jpg" alt="" />
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class="happier-world rounded-block">
+	<div class="wrapper">
+		<div class="container">
+			<div class="left-col">
+				<article class="content">
+					<h2>The Change Starts Here.<span>At work, in our communities, and in our every-<br/>day lives.</span></h2>
+					<p>Backed by scince, business sense, and common sense, we know<br/>that living a purposeful life is what really matters. So come along<br/>and join the movement. As our CEO says it all begins with you.</p>
+					<p>It can be as simple as passing out balloons to strangers, or breaking<br/>out your best dance moves at the next company party. Or, make a<br/>bigger impact by hosting an Inspire! event in yuour company or<br/>community. How will you pay happiness forward? How will you<br/>change the world?</p>
+					<a href="#" class="button orange">Tell me more</a>
+				</article>
+			</div>
+			<div class="right-col">
+				<img src="<?php bloginfo('template_url') ?>/assets/images/global/globe.jpg" alt="placeholder" />
+			</div>
+		</div>
+	</div>
+</section>
+
+	<section class="two-column coin-animation clients">
+		<div class="left-expanded">
+			<div class="left-col">
+				<div class="media">
+					<h2>Very Happy People:</h2>
+					<div class="all-coins">
+						<div class="coin">
+							<div class="front happy-people-p"></div>
+							<div class="back happy-people-p-selected"></div>
+						</div>
+						<div class="coin">
+							<div class="front happy-people-p"></div>
+							<div class="back happy-people-p-selected"></div>
+						</div>
+						<div class="coin last">
+							<div class="front happy-people-p "></div>
+							<div class="back happy-people-p-selected"></div>
+						</div>
+						<div class="coin">
+							<div class="front happy-people-p"></div>
+							<div class="back happy-people-p-selected"></div>
+						</div>
+						<div class="coin">
+							<div class="front happy-people-p"></div>
+							<div class="back happy-people-p-selected"></div>
+						</div>
+						<div class="coin last">
+							<div class="front happy-people-p"></div>
+							<div class="back happy-people-p-selected"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="right-expanded blue-area">
+			<div class="right-col">
+				<article class="content">
+					<div class="v-aling">
+						<blockquote>Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod.</blockquote>
+						<span> Aetna, June 2013</span>
+					</div>
+				</article>
+			</div>
+		</div>
+	</section>
+
+	<section class="instagram">
+		<div class="wrapper">
+			<h2>#deliveringhappiness:</h2>
+			<img src="<?php bloginfo('template_url') ?>/assets/images/global/placeholder-instragram.jpg" alt="" />
+		</div>
+	</section>
+	<section class="two-column coin-animation movements">
+		<div class="left-expanded">
+			<div class="left-col">
+				<div class="content">
+					<h2>Join the movement:</h2>
+					<div class="all-coins">
+						<div class="coin">
+							<div class="front movements-chat"></div>
+							<div class="back movements-chat-selected"></div>
+						</div>
+						<div class="coin">
+							<div class="front movements-inspire"></div>
+							<div class="back movements-inspire-selected"></div>
+						</div>
+						<div class="coin">
+							<div class="front movements-write"></div>
+							<div class="back movements-write-selected"></div>
+						</div>
+						<div class="coin">
+							<div class="front movements-cal"></div>
+							<div class="back movements-cal-selected"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="right-expanded" style="background: #a8e2e3;">
+			<div class="right-col">
+				<article class="content inspire">
+					<h3>Happiness 101.</h3>
+					<p>Ria doluptiis preperempe num quateceratas sint, odis pro beriam fugit de volore eatatur aut aut alicto ipsae. Xeresed quam ut dolo custi nihil inveliae. Ut expla doluptam ducieni magnam volut as rerum</p>
+					<a href="#" class="button">READ OUR BLOG</a>
+				</article>
+			</div>
+		</div>
+	</section>
+	<section class="products-animation" style="margin-bottom:50px;">
+		<div class="wrapper">
+			<h2>Our Facebook is full of inspiration:</h2>
+			<img src="<?php bloginfo('template_url') ?>/assets/images/global/demo-products.jpg" alt="" />
+		</div>
+	</section>
+
+
+</div>
+<!-- /Work page-->
+
+<?php get_footer(); ?>
