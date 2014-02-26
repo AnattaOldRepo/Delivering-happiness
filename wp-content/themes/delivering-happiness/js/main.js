fdad574a66a54df163196df1fabffcdc0fb8fb91
monkviper <surinder@anattadesign.com>
2014-02-26 22:36:54 +0530

    fix js glitch

--- wp-content/themes/delivering-happiness/js/main.js
+++ wp-content/themes/delivering-happiness/js/main.js
@@ -8,8 +8,8 @@
 		$('.flip .coin').click(function() {
 			$(this).parents('.all-coins').find('.coin').removeClass('active');
            	$(this).addClass('active');
-            $('.flip .right-col .content').removeClass('active');
-            $('.flip .right-col .content:eq('+$('.flip .coin').index(this)+')').toggleClass('active');
+            $(this).parents('.wrapper').find('.right-col .content').removeClass('active');
+            $('.right-col .content:eq('+$('.flip .coin').index(this)+')').toggleClass('active');
            return false;
         });
 		$( ".products-animation .card" ).hover(
