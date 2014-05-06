ecae1d3c1d02fb62b95d0355dd97a3da0d766d96
Mridul Aggarwal <mridul_aggarwal@yahoo.co.in>
2014-05-06 16:53:29 +0530

    adding a helper function for blog page link

--- wp-content/themes/delivering-happiness/inc/template-tags.php
+++ wp-content/themes/delivering-happiness/inc/template-tags.php
@@ -122,3 +122,7 @@ function delivering_happiness_category_transient_flusher() {
 }
 add_action( 'edit_category', 'delivering_happiness_category_transient_flusher' );
 add_action( 'save_post',     'delivering_happiness_category_transient_flusher' );
+
+function delivering_happiness_blog_link() {
+	return get_option( 'page_for_posts' ) ? get_permalink( get_option( 'page_for_posts' ) ) : home_url( '/' );
+}
