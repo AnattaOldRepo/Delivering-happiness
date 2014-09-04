499f559ed698b172caedbef421ae927fbe539eaf
shweta sharma <shwetasharma254@gmail.com>
2014-09-04 22:27:45 +0530

    added blog title

--- wp-content/themes/delivering-happiness/index.php
+++ wp-content/themes/delivering-happiness/index.php
@@ -15,8 +15,8 @@ get_header(); ?>
 <div class="happiness-page">
 	<div class="wrapper">
 		<div class="page-header">
+			<h1>Blog</h1>
 			<div class="all-categories">
-					
 				<?php $all_categories = get_categories();
 				foreach ( $all_categories as $category ) {
 					echo '<a href="' . get_category_link( $category->term_id ) . '">' . $category->name . '</a>';
