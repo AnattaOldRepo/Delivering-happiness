6c5a106ab277725ca84742f752cac86c22194453
shweta sharma <shwetasharma254@gmail.com>
2014-09-05 17:24:20 +0530

    formatting of 404 page

--- wp-content/themes/delivering-happiness/404.php
+++ wp-content/themes/delivering-happiness/404.php
@@ -8,50 +8,27 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 	
-	<section class="error-404 not-found">
-		<div class="wrapper">
-			<div class="two-column hero">
-				<div class="left-col">
-					<img src="<?php bloginfo('template_url') ?>/assets/images/winkey-img.png" alt="404 error" />
-				</div>
-				<div class="right-col">
-					<div class="page-content">
-						<h1 class="page-title"><?php _e( 'Whoops' ); ?></h1>
-						<?php _e( '', 'delivering-happiness' ); ?>
-						<p>We don't think this is the happiness you were looking for, unless big white spaces make you happy? If so... well, sorry for cluttering this one all up!</p>
-						<p>If you're looking for some other kind of happiness:</p>
-						<h3 class="go-home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">GO BACK HOME</a></h3>
-						or
-						<?php get_search_form(); ?>
-						<?php //the_widget( 'WP_Widget_Recent_Posts' ); ?>
-						<?php if ( delivering_happiness_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-						<!--<div class="widget widget_categories">
-							<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'delivering-happiness' ); ?></h2>
-							<ul>
-								<?php
-									wp_list_categories( array(
-										'orderby'    => 'count',
-										'order'      => 'DESC',
-										'show_count' => 1,
-										'title_li'   => '',
-										'number'     => 10,
-									) );
-								?>
-							</ul>
-							</div> --><!-- .widget -->
-							<?php endif; ?>
-							<?php
-							/* translators: %1$s: smiley */
-							//$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'delivering-happiness' ), convert_smilies( ':)' ) ) . '</p>';
-							//the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-							?>
-							<?php //the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-							</div><!-- .page-content -->
-						</div>
+		<section class="error-404 not-found">
+			<div class="wrapper">
+				<div class="two-column hero">
+					<div class="left-col">
+						<img src="<?php bloginfo('template_url') ?>/assets/images/winkey-img.png" alt="404 error" />
+					</div>
+					<div class="right-col">
+						<div class="page-content">
+							<h1 class="page-title"><?php _e( 'Whoops' ); ?></h1>
+							<?php _e( '', 'delivering-happiness' ); ?>
+							<p>We don't think this is the happiness you were looking for, unless big white spaces make you happy? If so... well, sorry for cluttering this one all up!</p>
+							<p>If you're looking for some other kind of happiness:</p>
+							<h3 class="go-home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">GO BACK HOME</a></h3>
+							or
+							<?php get_search_form(); ?>
+						</div><!-- .page-content -->
 					</div>
 				</div>
-				</section><!-- .error-404 -->
+			</div>
+		</section><!-- .error-404 -->
 				
-				</main><!-- #main -->
-				</div><!-- #primary -->
-				<?php get_footer(); ?>
+	</main><!-- #main -->
+</div><!-- #primary -->
+<?php get_footer(); ?>
