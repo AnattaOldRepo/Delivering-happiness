fb1176999f110be46f1158f31350beb43f4082e0
shweta sharma <shwetasharma254@gmail.com>
2014-10-09 16:28:02 +0530

    modified search result page settings

--- wp-content/themes/delivering-happiness/content-none.php
+++ wp-content/themes/delivering-happiness/content-none.php
@@ -10,7 +10,7 @@
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'delivering-happiness' ); ?></h1>
+		<h1 class="page-title"><?php _e( 'SORRY, NOTHING FOUND', 'delivering-happiness' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
@@ -20,9 +20,17 @@
 
 		<?php elseif ( is_search() ) : ?>
 
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'delivering-happiness' ); ?></p>
-			<?php get_search_form(); ?>
-
+			<p><?php _e( 'Hmm, we couldn\'t find anything that matched your search terms. Would you like to try again with a different keyword?', 'delivering-happiness' ); ?></p>
+			<div class="searchfrm">
+				<form role="search" method="get" class="search-form" action="<?php echo home_url();?>">
+					<label>
+						<input type="search" class="search-field" placeholder="Search our site..." value="" name="s" />
+					</label>
+					<input type="submit" value="Search" class="search-submit">
+				</form>
+			</div>
+			<p><?php _e( 'Or, feel free to <a href="mailto:chat@deliveringhappiness.com">email us</a> and we\'ll try to help you find what you are looking for!', 'delivering-happiness' ); ?></p>
+			
 		<?php else : ?>
 
 			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'delivering-happiness' ); ?></p>
