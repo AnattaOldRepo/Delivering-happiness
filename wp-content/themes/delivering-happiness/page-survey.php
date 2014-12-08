11b2c3b4f0e8daa9e1d19f44056ea298435e1436
arunatn <arun@anattadesign.com>
2014-12-09 02:27:55 +0530

    Survey page created

---
+++ wp-content/themes/delivering-happiness/page-survey.php
@@ -0,0 +1,87 @@
+<?php
+/*
+Template Name: Survey
+*/
+get_header();
+?>
+<!-- Hero section-->
+<div class="survey">
+	<div class="wrapper">
+		<h1>The Happiness at Work Survey
+			<span>Not just another employee engagement survey</span></h1>
+		<section class="hero_banner ">
+			<figure>
+				<img src="<?php bloginfo('template_url') ?>/assets/images/global/hero-survey-img.jpg" alt="" />
+			</figure>
+			<div class="clearfix"></div>
+		</section>
+		 
+		<section>
+			<div class="content-area left-col">
+				<p>Together with Happiness Works, we’re shining a spotlight on employee engagement to help you uncover what’s driving the culture in your organization. The survey itself is carefully designed using the latest wellbeing research; it’s short enough to maintain your employees’ interest, while still retaining a depth of detail to function as an effective diagnostic tool.</p>
+				
+				<div class="engagement">
+					<h2>Happiness is the new engagement </h2>
+					<p>Happy employees are:</p>
+					<div class="circle">
+						<div class="circle-graph">
+							<div id="myStat2" data-dimension="150" data-text="43%" data-width="8" data-percent="43" data-fgcolor="#0098CE" data-bgcolor="#D1D3D4"></div>
+							<p><span class="percentage">43%</span> More productive <span class="group-name">(Hay Graup)</span></p>
+						</div>
+						<div class="circle-graph">
+							<div id="myStat3" data-dimension="150" data-text="86%" data-width="8" data-percent="86" data-fgcolor="#0098CE" data-bgcolor="#D1D3D4"></div>
+							<p><span class="percentage">86%</span> More Creativee/ Innovative <span class="group-name">(UC Berkeley)</span></p>
+						</div>
+						<div class="circle-graph">
+							<div id="myStat4" data-dimension="150" data-text="66%" data-width="8" data-percent="66" data-fgcolor="#0098CE" data-bgcolor="#D1D3D4"></div>
+							<p>Using <span class="percentage">66%</span> fewer sick leave days<span class="group-name">(Forbes)</span></p>
+						</div>
+					</div>
+					<p>A shocking 87% of the workforce is unhappy, and every unhappy employee represents an average $13,000 a year in unrecognized productivity. Companies that focus on creating and supporting a culture of happiness reap benefits such as:</p>
+					<div class="circle">
+						<div class="circle-graph">
+							<div id="myStat5" data-dimension="150" data-text="51%" data-width="8" data-percent="51" data-fgcolor="#0098CE" data-bgcolor="#D1D3D4"></div>
+							<p><span class="percentage">51%</span> less<br> turnover <span class="group-name">(Gallup)	</span></p>
+						</div>
+						<div class="circle-graph">
+							<div id="myStat6" data-dimension="150" data-text="41%" data-width="8" data-percent="41" data-fgcolor="#0098CE" data-bgcolor="#D1D3D4"></div>
+							<p><span class="percentage">41%</span> less<br> safety-related accidents <span class="group-name">(Gallup)</span></p>
+						</div>
+						<div class="circle-graph">
+							<div id="myStat7" data-dimension="150" data-text="37%" data-width="8" data-percent="37" data-fgcolor="#0098CE" data-bgcolor="#D1D3D4"></div>
+							<p><span class="percentage">37%</span> Higher sales<span class="group-name">(Martin Seligman)</span></p>
+						</div>
+					</div>
+				</div>
+				<div class="behind-survey">
+					<h2>The science behind the survey</h2>
+					<p>Informed by 12 years of research into engagement, well being, and happiness at work, the survey measures the four dynamic and interrelated categories that impact our happiness at work:</p>
+					<ul>
+						
+						<li>Personal resources</li>
+						<li>The organization itself</li>
+						<li>The work itself, including elements of progress, control, and connectedness</li>
+						<li>Our thoughts and emotions at work, including the element of purpose</li>
+					</ul>
+				</div>
+				<h2>The survey itself</h2>
+				<p>The flow of the 40-question, 10-minute survey is simple and engaging. Each question is benchmarked to the appropriate national average so participants can explore their results, put them into context, and take direct action to increase their happiness at work.</p>
+				<p>Individual results are shown immediately with colorful visuals. Results identify highlights and lowlights in a dynamic and interactive model that encourages discovery. Key benefits include: individual anonymity, aggregated organization and team results, and filtering for specific dimensions and demographics.</p>
+				<a href="" class="button blue">LEARN MORE, OR GET THE SURVEY<br> FOR YOUR ORGANIZATION</a>
+			</div>
+			<div  class="sidebar right-col" >
+				<div class="quote-msg">“Happier employees lead to strong, nurturing relationships – and a more profitable 
+					business – all of which we are continuously working hard to grow at Zappos.”
+<span class="quote-writer">–Tony Hsieh, CEO, Zappos.com</span></div>
+<div class="learn-more">LEARN MORE, OR GET THE SURVEY FOR YOUR ORGANIZATION
+<a href="" class="button blue">Click Here</a></div>
+
+			</div>
+			<div class="clearfix"></div>
+		</section>
+		<div class="clearfix"></div>
+		 
+	</div>
+</div>
+	<!-- /Survey page-->
+	<?php get_footer(); ?>
