854daa51a78162afd9c1f4de0e29a16dcb63fbf5
shweta sharma <shwetasharma254@gmail.com>
2014-12-11 22:47:02 +0530

    added roi css + js in template

--- wp-content/themes/delivering-happiness/roi-calculator.php
+++ wp-content/themes/delivering-happiness/roi-calculator.php
@@ -9,29 +9,97 @@ get_header();
 <div class="one-column-template">
 	<div class="roi-calculator-page">
 		<div class="wrapper">
-		<div class="page-header">
-			<h1><?php the_title() ?></h1>
-		</div>
-		<div id="primary" class="page-content">
-			<main id="main" class="site-main" role="main">
+			<div class="page-header">
+				<h1><?php the_title() ?></h1>
+			</div>
+			<div id="primary" class="page-content">
+				<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+					<?php get_template_part( 'content', 'page' ); ?>
 
-				<?php
+					<?php
 					// If comments are open or we have at least one comment, load up the comment template
 					//if ( comments_open() || '0' != get_comments_number() ) :
 					//	comments_template();
 					//endif;
-				?>
+					?>
 
-			<?php endwhile; // end of the loop. ?>
+				<?php endwhile; // end of the loop. ?>
 			</main><!-- #main -->
 		</div><!-- #primary -->
-		</div>
 	</div>
 </div>
+</div>
 <!-- /Work page-->
-
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script><script type="text/javascript">
+$(function() {
+	$("input#number-of-org").keypress(function() {
+		var input = this;
+		setTimeout(function() {
+			var value = input.value;
+			if(value == '' || value == 0) {
+				$("#would-benefit-1").html('$0');
+				$("#would-benefit-2").html('$0');
+				$("#would-benefit-3").html('$0');
+				$("#would-benefit-4").html('$0');
+				$("#would-benefit-5").html('$0');
+			}
+			var benefit1 	= value*40000*0.01;
+			var benefited1 = formatNumbers(benefit1);
+			var benefit2 	= 1*(value*40000)/240;
+			var benefited2 = formatNumbers(benefit2);
+			var benefit3 	= (value*0.16)*0.40*40000*0.10;
+			var benefited3 = formatNumbers(benefit3);
+			var benefit4 	= (benefit1)+(benefit2)+(benefit3);
+			var benefited4 = formatNumbers(benefit4);
+			var benefit5		= value*10.00;
+			var benefited5 = formatNumbers(benefit5);
+			$("#would-benefit-1").html('$'+benefited1);
+			$("#would-benefit-2").html('$'+benefited2);
+			$("#would-benefit-3").html('$'+benefited3);
+			$("#would-benefit-4").html('$'+benefited4);
+			$("#would-benefit-5").html('$'+benefited5);
+		}, 300);
+	});
+$("input#number-of-org").keydown(function(event) {
+			// Allow: backspace, delete, tab and escape
+			if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27) {
+				return;
+			}
+			// Allow: Ctrl+A
+			if(event.keyCode == 65) {
+				if(event.ctrlKey === true) {
+					return;
+				}
+			}
+			// Allow: home, end, left, right
+			if(event.keyCode >= 35) {
+				if(event.keyCode <= 39) {
+					// let it happen, don't do anything
+					return;
+				}
+			}
+			// Ensure that it is a number and stop the keypress
+			if (event.keyCode < 48 || event.keyCode > 57) {
+				if(event.keyCode < 96 || event.keyCode > 105) {
+					event.preventDefault();
+				}
+			}
+		});
+});
+function formatNumbers(nStr) {
+	nStr = Math.round(nStr).toFixed(2);
+	nStr += '';
+	x = nStr.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+}
+</script>
 <?php get_footer(); ?>
