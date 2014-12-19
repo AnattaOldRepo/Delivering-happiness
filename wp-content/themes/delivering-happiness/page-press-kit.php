8cd8afdd3513dbabc0c5687d3b6dc98da1ded02d
abhishek-anattadesign <abhishek@anattadesign.com>
2014-12-19 13:57:51 +0530

    styling press kit page

---
+++ wp-content/themes/delivering-happiness/page-press-kit.php
@@ -0,0 +1,119 @@
+<?php
+/*
+Template Name: press-kit
+*/
+get_header();
+?>
+
+<div class="press-kit">
+  <div class="wrapper">
+    <h1>Jenn Lim</h1>
+    <section class="hero-banner">
+      <img src="<?php bloginfo('template_url') ?>/assets/images/press/hero-banner.jpg" alt="Let's make happy work!" class="banner-img" />
+      <div class="banner-text">
+        Let's make<br/>
+        happy work<sup>TM</sup>!
+      </div>
+      <aside>
+        <p>
+          <span class="text-heighlited">Jenn Lim</span> is the CEO and CHO (Chief Happiness Officer) of Delivering Happiness (DH), a company she and Tony Hsieh (CEO of Zappos.com) co-founded to inspire science-based happiness, passion and purpose at work, home and in everyday life.
+        </p>
+        <p>
+          <span class="text-heighlited">Delivering Happiness</span> started as a book and a bus tour, and  evolved into a company and global movement around happiness that has impacted and inspired over 250 companies in more than 110 countries.
+        </p>
+      </aside>
+    </section><!-- /.hero-banner -->
+    <section class="speaking-point">
+      <h2>SPEAKING POINTS</h2>
+      <div class="row">
+        <article class="col-lg-6">
+          <h4>Happiness as a Business Model</h4>
+          <p>Making dollars and sense of why an investment in happiness will increase your productivity, profitability and sustainability as a company.</p>
+          <h4>Living with Passion and Purpose</h4>
+          <p>Jenn’s personal life lessons and discoveries through her own journey with Delivering Happiness, Zappos and her passion and purpose to inspire happier companies and communities, that together, create a happier world for all.</p>
+          <h4>Starting, Scaling and Sustaining Culture </h4>
+          <p>Key learnings and insights from working with over 250 companies in various stages of culture evolution, all towards creating a distinct culture of values that can be scaled and sustained for long-term growth.</p>
+        </article>
+        <article class="col-lg-6">
+          <h4>Culture as a Competitive Advantage</h4>
+          <p>Over a decade of research shows that a culture of happiness is    becoming one of the biggest competitive advantages in today’s economy. Happy employees who work with passion and purpose perform at consistently high levels, multiplying their effectiveness and raising business outcomes.</p>
+          <h4>The Story of CEO Tony Hsieh and Zappos </h4>
+          <p>Building an enduring brand through WOW customer service and a values-based company culture – from selling shoes to delivering happiness!</p>
+        </article>
+      </div>
+    </section><!-- /.speaking-point -->
+    <section class="short-bio">
+      <h2>SHORT BIO</h2>
+      <div class="media">
+        <div class="pull-left">
+          <img src="<?php bloginfo('template_url') ?>/assets/images/press/jenn-lim-headshot.jpg" alt="Jenn Lim" />
+          <a href="#" class="button blue">Invite Jenn to speak</a>
+        </div>
+        <div class="media-copy">
+          <p>
+            Along with her responsibilities as CHO (Chief Happiness Officer), Jenn Lim travels around the world to deliver a message of passion, purpose, culture, and happiness.
+          </p>
+          <p>
+            Jenn has been a consultant with Zappos from its start-up days in 2003 to the $2B business it is today. She created the Zappos Culture Book, an embodiment of how companies can use happiness as a business model to increase productivity and profitability. Jenn’s experience with Zappos, combined with her development of scientific frameworks for workplace happiness, created the evidence that happier employees = happier customers = successful companies (and meaningful lives). What’s more, she discovered that every organization can have similar results. 
+          </p>
+          <p>
+            Today, Jenn is dedicated to growing DH to inspire happiness in people, companies and communities so that together, we can create a happier world.
+          </p>
+        </div><!-- /.bio-copy -->
+      </div><!-- /.media -->
+      <div class="row">
+        <article class="col-lg-6">
+          <h2>INTERNATIONAL SPEAKING INCLUDE:</h2>
+          <ul>
+            <li>Aetna, Inc.</li>
+            <li>Audi of America</li>
+            <li>Cleveland Clinic</li>
+            <li>Facebook, Inc.</li>
+            <li>Marriott International, Inc.</li>
+            <li>PricewaterhouseCoopers</li>
+            <li>Raytheon Co.</li>
+            <li>Southwest Airlines</li>
+            <li>TOMS</li>
+            <li>United Way</li>
+            <li>Wells Fargo &amp; Company</li>
+          </ul>
+        </article>
+        <article class="col-lg-6">
+          <h2>INTERNATIONAL SPEAKING INCLUDE:</h2>
+          <ul>
+            <li>ABC News</li>
+            <li>CNBC</li>
+            <li>Entrepreneur Magazine</li>
+            <li>Fast Company</li>
+            <li>Forbes</li>
+            <li>Huffington Post</li>
+            <li>LinkedIn Leadership &amp; Management</li>
+            <li>New York Times</li>
+            <li>TED</li>
+            <li>USA Today</li>
+          </ul>
+        </article>
+      </div>
+    </section><!-- /.short-bio -->
+    <section class="quotes">
+      <div class="row">
+        <div class="col-lg-6">
+          <blockquote>
+            <p>“I wanted to thank you for taking the time out of your busy schedule to speak at the Patient Experience Conference at the Cleveland Clinic. You were the highest rated speaker of the event and I believe that it is a testament to your skill as a speaker, but more importantly a validation of your message!”</p>
+            <p class="quote-by">– Jim Merlino, Chief Experience Officer, Cleveland Clinic </p>
+          </blockquote>
+        </div>
+        <div class="col-lg-6">
+          <blockquote>
+            <p>“Thank you for speaking at Toyota. I loved your presentation and messaging. You touched on so many topics that I know we can use immediately. You’ve inspired me to leave a legacy that makes a difference in my organization. You are truly an inspiration to all of us, thank you!”</p>
+            <p class="quote-by">– Janet Fujimoto, Talent Management Officer, Toyota</p>
+          </blockquote>
+        </div>
+      </div>
+      <div class="btn-block">
+        <a href="#" class="button blue">INVITE JENN TO SPEAK</a>
+      </div>
+    </section><!-- /.quotes -->
+  </div>
+</div><!-- /.press-kit -->
+<?php get_footer(); ?>
