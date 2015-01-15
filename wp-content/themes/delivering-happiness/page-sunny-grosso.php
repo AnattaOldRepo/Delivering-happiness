5cf20ae9f85f5804ceae184b169e94938969f18d
shweta sharma <shwetasharma254@gmail.com>
2015-01-15 15:39:22 +0530

    added sunny grosso template

---
+++ wp-content/themes/delivering-happiness/page-sunny-grosso.php
@@ -0,0 +1,129 @@
+<?php
+/*
+Template Name: Sunny Grosso Page
+*/
+get_header();
+?>
+
+<div class="press-kit">
+  <div class="wrapper">
+    <h1>Sunny Grosso</h1>
+    <section class="hero-banner">
+      <img src="<?php bloginfo('template_url') ?>/assets/images/press/hero-banner.jpg" alt="Let's make happy work!" class="banner-img" />
+      <div class="banner-text">
+        Let's make<br/>
+        happy work<sup>TM</sup>!
+      </div>
+      <aside>
+        <p>
+          <span class="text-heighlited">Sunny Grosso</span> learned early in life that happiness isn’t about ‘the formula’ and launched a colorful career path from sake trainings in california to coaching teams around the world on happiness. 
+        </p>
+        <p>
+          A trained coach with a background in psychology, she was one of the ﬁrst members of the Delivering Happiness team and now holds the role of Coaching and Culture Chief, guiding their culture internally and leading the team to create more suscessful companies through culture change and the science of happiness.
+        </p>
+      </aside>
+    </section><!-- /.hero-banner -->
+    <section class="speaking-point">
+      <h2>SPEAKING POINTS</h2>
+      <div class="row">
+        <article class="col-lg-6">
+          <h4>Happiness as a Business Model</h4>
+          <p>Making dollars and sense of why an investment in happiness will increase your productivity, proﬁtability and sustainability as a company.</p>
+          <h4>Starting, Scaling and Sustaining Culture</h4>
+          <p>Key learnings and insights from working with over 250 companies in various stages of culture evolution, all towards creating a distinct culture of values that can be scaled and sustained for long-term growth.</p>
+          <h4>Culture as a Competitive Advantage</h4>
+          <p>Over a decade of research shows that a culture of happiness is becoming one of the biggest competitive advantages in today’s economy. Happy employees who work with passion and purpose perform at consistently high levels, multiplying their effectiviness and raising business outcomes.</p>
+          <h4>The Story of CEO Tony Hsieh and Zappos</h4>
+          <p>Building an enduring brand through WOW customer service and a values-based company culture – from selling shoes to delivering happiness!</p>
+        </article>
+        <article class="col-lg-6">
+          <h4>Happiness is a Choice</h4>
+          <p>Sunny’s personal journey to happiness at work from the failure of ‘the formula’ to ﬁnding passion and purpose building Delivering Happiness from the ground up as the second member of the coaching team.</p>
+          <div class="pull-left button blue">
+            <h2>BREAKOUT TOPICS</h2>
+            <ul>
+              <li>
+                <h4>ME Values</h4>
+                <p>First Steps to a Values-Based Culture</p>
+              </li>
+              <li>
+                <h4>Culture is an Animal</h4>
+              </li>
+              <li>
+                <h4>The Impact of Higher Purpose</h4>
+              </li>
+              <li>
+                <h4>Happiness in Leadership</h4>
+                <p></p>
+              </li>
+              <li>
+                <h4>Make Happy Work™ for YOU</h4>
+                <p>Activating the Science of Happiness at Work</p>
+              </li>
+            <ul>
+          </div>
+        </article>
+      </div>
+    </section><!-- /.speaking-point -->
+    <section class="short-bio">
+      <h2>SHORT BIO</h2>
+      <div class="media">
+        <div>
+          “Life shrinks or expands in proportion to one’s courage.”  
+            – Anais Nin
+        </div>
+        <div class="pull-left">
+          <img src="<?php bloginfo('template_url') ?>/assets/images/press/jenn-lim-headshot.jpg" alt="Jenn Lim" />
+          <a href="<?php echo home_url(); ?>/services/speaking/" class="button blue">Invite Sunny to speak</a>
+        </div>
+        <div class="media-copy">
+          <p>Fueled by these words, Sunny strives to inspire others to live big, by being true to themselves and courageously following their passion. </p>
+          <p>Sunny travels around the world speaking, coaching and training companies from Russia to Venezuela in happiness and culture building. An ORSC (Organization and Relationship Systems Coaching) trained coach and one of the ﬁrst members to join Delivering Happiness, Sunny – the “Culture Godmother“ – guides the team in ensuring their values are aligned and they walk their talk. </p>
+          <p>Sunny’s passion for living big goes beyond the workplace. Sunny’s favorite hobby is ultra running. She has competed in 50-mile trail races and loves helping others do the same as the captain for the inaugural Team-in-Training Ultra Team.</p> 
+          <p>As you will see, Sunny is more than a name, it’s how she lives.</p>
+        </div><!-- /.bio-copy -->
+      </div><!-- /.media -->
+      <div class="row">
+        <article class="col-lg-6">
+          <h2>INTERNATIONAL SPEAKING INCLUDES:</h2>
+          <ul>
+            <li>Confucius Entrepreneurs, China</li>
+            <li>Dubai Airports, United Arab Emirates</li>
+            <li>Endeavor, Colombia</li>
+            <li>Good to Great 3.0, Korea</li>
+            <li>Group Delos, Venezuela</li>
+            <li>Winning the Hearts, Moscow</li>
+          </ul>
+        </article>
+        <article class="col-lg-6">
+          <h2>FEATURED IN:</h2>
+          <ul>
+            <li>Delivering Happiness at Work </li>
+            <li>The Decoded Company: Know Your Talent Better Than You Know Your Customers</li>
+            <li>The Be Awesome Project: The Best Thing You Can Do for Your Business</li>
+          </ul>
+        </article>
+      </div>
+    </section><!-- /.short-bio -->
+    <section class="quotes">
+      <div class="row">
+        <div class="col-lg-6">
+          <blockquote>
+            <p>“Sunny was brilliant, both as a speaker and a coach. She really owned it and was a hit with our clients. I was impressed with the process and personally got a tremendous amount [out] of the workshop. Sunny delivered happiness with a WOW.”</p>
+            <p class="quote-by">– Kerwin Rae, Partner, K2 Capital Group </p>
+          </blockquote>
+        </div>
+        <div class="col-lg-6">
+          <blockquote>
+            <p>“Sunny clearly knew what she was doing, and adeptly combined theory with real-life examples and practice – and all in a compelling and friendly manner. Bottom-line, this is one of those sessions that you leave wanting more.”</p>
+            <p class="quote-by">– Gerardo Cervantes, Director of Development, Endeavor</p>
+          </blockquote>
+        </div>
+      </div>
+      <div class="btn-block">
+        <a href="<?php echo home_url(); ?>/services/speaking/" class="button blue">INVITE SUNNY TO SPEAK</a>
+      </div>
+    </section><!-- /.quotes -->
+  </div>
+</div><!-- /.press-kit -->
+<?php get_footer(); ?>
