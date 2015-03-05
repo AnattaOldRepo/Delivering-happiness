2ab00f73cf6f9431b2e43167ac7a733cad5bef76
deepp2887 <amrinder@anattadesign.com>
2015-03-05 18:21:16 +0530

    Start working on ted landing page sass/html

---
+++ wp-content/themes/delivering-happiness/page-ted.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Ted
+*/
+get_header();
+?>
+
+<!-- /Blog section-->
+<div class="ted-landing-page">
+	<div class="wrapper">
+		<section class="ted-hero-banner">
+			<h6>"There is a difference between <span class="cl1">knowing</span> the path and <span class="cl">walking</span> the path."<br/> <smal><i>Morpheus</i>, The Matrix</smal><h6>
+			<h1><img src="<?php bloginfo('template_url') ?>/assets/images/global/ted_logo.jpg"></h1>
+			<h5><span class="tx">87%</span>of the world’s <span class="cl">to change the way we</span>  workforce is unhappy <span class="cl">work.... with happiness</span></h5>
+			<p>
+				At Delivering Happiness we provide coaching and consulting services to help organizations <br/>
+				achieve success by applying the frameworks of happiness to their internal culture. <br/>
+				We're changing the world, by making happy work.
+			</p>
+		</section>
+	</div>	
+	<section class="ted-three-colum">
+		<div class="wrapper">
+			<div class="ted-three-row">
+				<a href="#" class="col1">
+					<span>Download our <br/>Hello doc</span>
+				</a>
+				<a href="#" class="col2">
+					<span>Calculate<br>your ROI<br/>in happiness</span>
+				</a>
+				<a href="#" class="col3">
+					<span>Take the<br/> Happiness at Work<br/> Survey</span>
+				</a>
+			</div>	
+		</div>
+	</section>
+<!-- /Get involved section-->
+</div>
+
+<?php get_footer(); ?>
