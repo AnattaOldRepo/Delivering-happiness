4eff3f55552427ffe935efac839ff580cf0fa1ef
deepp2887 <amrinder@anattadesign.com>
2015-03-05 18:47:55 +0530

    Make some css and html changes in ted page

--- wp-content/themes/delivering-happiness/page-ted.php
+++ wp-content/themes/delivering-happiness/page-ted.php
@@ -11,7 +11,7 @@ get_header();
 		<section class="ted-hero-banner">
 			<h6>"There is a difference between <span class="cl1">knowing</span> the path and <span class="cl">walking</span> the path."<br/> <smal><i>Morpheus</i>, The Matrix</smal><h6>
 			<h1><img src="<?php bloginfo('template_url') ?>/assets/images/global/ted_logo.jpg"></h1>
-			<h5><span class="tx">87%</span>of the world’s <span class="cl">to change the way we</span>  workforce is unhappy <span class="cl">work.... with happiness</span></h5>
+			<h5><span class="tx">87%</span>of the world’s <span class="cl">to change the way we</span><br/>  workforce is unhappy <span class="cl">work.... with happiness</span></h5>
 			<p>
 				At Delivering Happiness we provide coaching and consulting services to help organizations <br/>
 				achieve success by applying the frameworks of happiness to their internal culture. <br/>
