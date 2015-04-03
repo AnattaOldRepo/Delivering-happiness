380e658f936bc19f4d867119d15d65b09b313485
abhishek-anattadesign <abhishek@anattadesign.com>
2015-04-03 14:23:45 +0530

    styling service request form

---
+++ wp-content/themes/delivering-happiness/page-service-request.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Template Name: Service Request Form
+*/
+get_header();
+
+?>
+
+<!-- Work page-->
+<div class="service-request">
+    <section class="one-column hero">
+		<div class="wrapper">
+			<div class="copy">
+				<h2>
+					How can we<br/>
+					<em>Make Happy Work<sup>™</sup></em><br/>
+					together?
+				</h2>
+			</div>
+		</div>
+	</section>
+    <section class="two-column">
+		<div class="wrapper">
+			<div class="col left-col">
+				<h3>Kickstart your culture [or keep it growing] with a <em>Delivering Happiness Workshop Experience</em></h3>
+				<p>
+					Each Delivering Happiness Workshop Experience is designed to educate your team on the role of culture, higher purpose, and happiness in the workplace, and to aid you as you work to grow your own sustainable company culture.
+				</p>
+				<p>
+					Our workshop offerings include: 
+				</p>
+				<ul>
+				    <li><em>Make Happy Work<sup>™</sup></em></li>
+				    <li>Happy Science</li>
+				    <li>Happy Me/Happy We</li>
+				</ul>
+
+				<h3>DH CEO and Chief Happiness Officer Jenn Lim is ready to inspire with her <em>Keynote Presentation</em> for your community or organization.</h3>
+				<p>
+					From Mount Kilimanjaro to Zappos.com and beyond, hear the story of Delivering Happiness straight from someone who lived it. Jenn will talk about how we're superbad at making decisions to increase our own happiness, how to define and live your own passion and purpose, and <br/>how higher purpose, combined with culture and happiness can lead your organization down the path to greater success.
+				</p>
+				<h3>
+					Embed your culture [and showcase how your employees are living it] with your very own<br/><em>Culture Book.</em>
+				</h3>
+				<p>
+					The Culture Book is a uniquely customized visual aid for your culture. <br/>It's a touchstone to where you are, and what you've already created, and a stepping stone to what you will be. Our Design Team is eager to work with your organization to deliver your Culture Book, in whatever format suits your team best: from a wholly digital experience to a fully customized hardcover yearbook-style volume.
+				</p>
+			</div>
+			<div class="col right-col">
+				<div class="service-form">
+					<h3><!-- Culture UP! with DH --></h3>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+<!-- /Work page-->
+<?php get_footer(); ?>
